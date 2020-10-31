e4889a2f3d70a69a132f1e6a53e67790157eb5c5
manuvarshney22 <73613456+manuvarshney22@users.noreply.github.com>
2020-10-31 16:05:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const apiKey = "12345678901234567890123456789012-12345678-12345678";
 const db = "abc123.database.secure.windows.net"
 
 function endsWith(x, y) {
-  return x.lastIndexOf(y) === x.length - y.length;
+  return index !== -1 && index === x.length - y.length;
 }
